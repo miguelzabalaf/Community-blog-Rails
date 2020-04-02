994637b6b6ed8159dc74a98d86c20c897536d586
miguelzabalaf <zabalafmiguel@hotmail.com>
2020-04-01 23:29:26 -0500

    He agregado a la vista show del user los artículos que ha creado, próximamanete arreglaré el layout de esta vista

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
   def show
     @user = User.find(params[:id])
+    @articles = Article.all
   end
 
   def edit

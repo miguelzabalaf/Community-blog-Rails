4233312adc963ed9b3e99363668ccfa0b334c0bd
miguelzabalaf <zabalafmiguel@hotmail.com>
2020-04-03 15:23:29 -0500

    He validado campos al registrarse y arreglado bug de cuando no hay artículos en artículos/index

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   #Index
   default_scope { order(updated_at: :desc)} #COLOCAR LOS MÁS ACTUALIZADOS DE PRIMERO
   #Auntenticaciones de los usuarios en Devise
+  validates :name, :lastname, :age, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
